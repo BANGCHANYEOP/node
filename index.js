098175c74daa4307656f056ed270cda6146fc2ff
CHANYEOP BANG <CHANYEOP BANG>
2017-03-31 22:56:03 +0900

    테스트2

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require("express")
 app = express();
 
-app.set("port" , (5000));
+app.set("port" , (80));
 app.listen(app.get("port") , 
   function(){ 
    console.log("APP IS RUNNING ON ["+ app.get("port") +"]");
  });
  app.get("/" , function(req , res)
- { res.send("Hello World!"); 
- });
+ { res.send("Hello World!");
+ { res.send("fuck you")} ;
+});
