58e06bf3f2a2973e1e8f463a25c839ff2d724e15
CHANYEOP BANG <CHANYEOP BANG>
2017-04-15 01:13:36 +0900

    router 추가

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-var express = require("express")
-app = express();
+var express = require('express');
+var path = require('path');
+var app = express();
 
-app.set("port" , (5000));
-app.listen(app.get("port") , 
-  function(){ 
-   console.log("APP IS RUNNING ON ["+ app.get("port") +"]");
- });
- app.get("/" , function(req , res)
- { res.send("Hello World!"); 
+app.set("view engine", 'ejs');
+
+var data={count:0};
+app.get('/', function (req,res) {
+  data.count++;
+  res.render('my_first_ejs',data);
+});
+app.get('/reset', function (req,res) {
+  data.count=0;
+  res.render('my_first_ejs',data);
+});
+app.get('/set/count', function (req,res) {
+  if(req.query.count) data.count=req.query.count;
+  res.render('my_first_ejs',data);
+});
+app.get('/set/:num', function (req,res) {
+  data.count=req.params.num;
+  res.render('my_first_ejs',data);
+});
+
+app.listen(3000, function(){
+  console.log('Server On!');
 });
